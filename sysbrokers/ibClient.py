18ac28f154d0d531930ad26c66e210fac3260649
Todd Gibson <tgibson11@gmail.com>
2017-06-25 15:17:28 -0700

    Added ability to get account & position data from IB

--- sysbrokers/ibClient.py
+++ sysbrokers/ibClient.py
@@ -1,7 +1,10 @@
-from sysbrokers.baseClient import brokerClient
+import queue
+
 from ibapi.client import EClient
 
-import queue
+from sysbrokers.baseClient import brokerClient
+from sysbrokers.ibUtils import simpleCache, finishableQueue, list_of_identified_items, \
+    ACCOUNT_UPDATE_FLAG, ACCOUNT_VALUE_FLAG
 
 
 class ibClient(EClient, brokerClient):
@@ -15,6 +18,10 @@ class ibClient(EClient, brokerClient):
     def __init__(self, wrapper):
         # Set up with a wrapper inside
         EClient.__init__(self, wrapper)
+        # We use these to store accounting data
+        self._account_cache = simpleCache(max_staleness_seconds=5*60)
+        # override function
+        self._account_cache.update_data = self._update_accounting_data
 
     def speakingClock(self):
         """
@@ -42,3 +49,77 @@ class ibClient(EClient, brokerClient):
             print(self.wrapper.get_error())
 
         return current_time
+
+    def get_current_positions(self):
+        """
+        Current positions held
+        :return:
+        """
+
+        # Make a place to store the data we're going to return
+        positions_queue = finishableQueue(self.wrapper.init_positions())
+
+        # ask for the data
+        self.reqPositions()
+
+        # poll until we get a termination or die of boredom
+        positions_list = positions_queue.get(timeout=10)
+
+        while self.wrapper.is_error():
+            print(self.wrapper.get_error())
+
+        if positions_queue.timed_out():
+            print("Exceeded maximum wait for wrapper to confirm finished whilst getting positions")
+
+        return positions_list
+
+    def _update_accounting_data(self, accountName):
+        """
+        Update the accounting data in the cache
+        :param accountName: account we want to get data for
+        :return: nothing
+        """
+
+        # Make a place to store the data we're going to return
+        accounting_queue = finishableQueue(self.wrapper.init_accounts(accountName))
+
+        # ask for the data
+        self.reqAccountUpdates(True, accountName)
+
+        # poll until we get a termination or die of boredom
+        accounting_list = accounting_queue.get(timeout=10)
+
+        while self.wrapper.is_error():
+            print(self.wrapper.get_error())
+
+        if accounting_queue.timed_out():
+            print("Exceeded maximum wait for wrapper to confirm finished whilst getting accounting data")
+
+        # separate things out, because this is one big queue of data with different things in it
+        accounting_list = list_of_identified_items(accounting_list)
+        separated_accounting_data = accounting_list.separate_into_dict()
+
+        # update the cache with different elements
+        self._account_cache.update_cache(accountName, separated_accounting_data)
+
+        # return nothing, information is accessed via get_... methods
+
+    def get_accounting_values(self, accountName):
+        """
+        Get the accounting values from IB server
+        :return: accounting values as served up by IB
+        """
+
+        # All these functions follow the same pattern: check if stale, if not return cache, else update values
+
+        return self._account_cache.get_updated_cache(accountName, ACCOUNT_VALUE_FLAG)
+
+    def get_accounting_updates(self, accountName):
+        """
+        Get the accounting updates from IB server
+        :return: accounting updates as served up by IB
+        """
+
+        # All these functions follow the same pattern: check if stale, if not return cache, else update values
+
+        return self._account_cache.get_updated_cache(accountName, ACCOUNT_UPDATE_FLAG)
--- sysbrokers/ibServer.py
+++ sysbrokers/ibServer.py
@@ -1,4 +1,5 @@
 from sysbrokers.baseServer import brokerServer
+from sysbrokers.ibUtils import identified_as, finishableQueue, ACCOUNT_VALUE_FLAG, ACCOUNT_UPDATE_FLAG
 from ibapi.wrapper import EWrapper
 
 import queue
@@ -14,10 +15,12 @@ class ibServer(EWrapper, brokerServer):
 
     def __init__(self):
         super().__init__()
-        error_queue = queue.Queue()
-        self._my_errors = error_queue
-        time_queue = queue.Queue()
-        self._time_queue = time_queue
+        self._my_errors = queue.Queue()
+        self._time_queue = queue.Queue()
+        # use a dict as could have different accountids
+        self._my_accounts = {}
+        # We set these up as we could get things coming along before we run an init
+        self._my_positions = queue.Queue()
 
     # Error handling code
     def get_error(self, timeout=5):
@@ -34,7 +37,7 @@ class ibServer(EWrapper, brokerServer):
         return an_error_if
 
     def error(self, id, errorCode, errorString):
-        # Overriden method
+        # Overridden method
         errormsg = "IB error id %d errorcode %d string %s" % (id, errorCode, errorString)
         self._my_errors.put(errormsg)
 
@@ -43,5 +46,41 @@ class ibServer(EWrapper, brokerServer):
         return self._time_queue
 
     def currentTime(self, time_from_server):
-        # Overriden method
+        # Overridden method
         self._time_queue.put(time_from_server)
+
+    # get positions code
+    def init_positions(self):
+        positions_queue = self._my_positions = queue.Queue()
+        return positions_queue
+
+    def position(self, account, contract, position, avgCost):
+        # uses a simple tuple, but you could do other, fancier, things here
+        position_object = (account, contract, position, avgCost)
+        self._my_positions.put(position_object)
+
+    def positionEnd(self):
+        # overridden method
+        self._my_positions.put(finishableQueue.FINISHED)
+
+    # get accounting data
+    def init_accounts(self, accountName):
+        accounting_queue = self._my_accounts[accountName] = queue.Queue()
+        return accounting_queue
+
+    def updateAccountValue(self, key: str, val: str, currency: str, accountName: str):
+        # use this to separate out different account data
+        data = identified_as(ACCOUNT_VALUE_FLAG, (key, val, currency))
+        self._my_accounts[accountName].put(data)
+
+    def updatePortfolio(self, contract, position: float,
+                        marketPrice: float, marketValue: float,
+                        averageCost: float, unrealizedPNL: float,
+                        realizedPNL: float, accountName: str):
+        # use this to separate out different account data
+        data = identified_as(ACCOUNT_UPDATE_FLAG, (contract, position, marketPrice, marketValue, averageCost,
+                                                   unrealizedPNL, realizedPNL))
+        self._my_accounts[accountName].put(data)
+
+    def accountDownloadEnd(self, accountName: str):
+        self._my_accounts[accountName].put(finishableQueue.FINISHED)
