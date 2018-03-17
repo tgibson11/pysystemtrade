aa03cf376ab566645fdd8444576e3264d43d7e19
Todd Gibson <tgibson11@gmail.com>
2018-03-17 12:44:10 -0700

    Re-organized private config files

--- sysinit/futures/instruments_csv_mongo.py
+++ sysinit/futures/instruments_csv_mongo.py
@@ -8,7 +8,7 @@ NOTE: THIS DIFFERS FROM 'INSTRUMENTS_CSV_MONGO' AS IT USES THE CONFIG FILE IN  '
 from sysdata.mongodb.mongo_futures_instruments import mongoFuturesInstrumentData
 from sysdata.csv.csv_instrument_config import csvFuturesInstrumentData
 
-INSTRUMENT_CONFIG_PATH = "private.my_system"
+INSTRUMENT_CONFIG_PATH = "private.my_system.config"
 
 data_out = mongoFuturesInstrumentData()
 data_in = csvFuturesInstrumentData(config_path=INSTRUMENT_CONFIG_PATH)
