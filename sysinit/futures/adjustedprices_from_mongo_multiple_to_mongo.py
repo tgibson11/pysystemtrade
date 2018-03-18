32b2de9556c9e460a1fb0970888bc3bc317052f5
Todd Gibson <tgibson11@gmail.com>
2018-03-18 12:25:50 -0700

    Moved adjusted prices script to method

--- sysinit/futures/adjustedprices_from_mongo_multiple_to_mongo.py
+++ sysinit/futures/adjustedprices_from_mongo_multiple_to_mongo.py
@@ -11,18 +11,23 @@ from sysdata.arctic.arctic_adjusted_prices import arcticFuturesAdjustedPricesDat
 from sysdata.futures.adjusted_prices import futuresAdjustedPrices
 
 
-if __name__ == '__main__':
+def generate_adjusted_prices():
     arctic_multiple_prices = arcticFuturesMultiplePricesData()
     artic_adjusted_prices = arcticFuturesAdjustedPricesData()
 
     instrument_list = arctic_multiple_prices.get_list_of_instruments()
 
     for instrument_code in instrument_list:
-        print(instrument_code)
+        print("Generating back-adjusted prices for " + instrument_code)
 
         multiple_prices = arctic_multiple_prices.get_multiple_prices(instrument_code)
         adjusted_prices = futuresAdjustedPrices.stich_multiple_prices(multiple_prices)
 
-        print(adjusted_prices)
+        # print(adjusted_prices)
 
+        artic_adjusted_prices.delete_adjusted_prices(instrument_code=instrument_code, are_you_sure=True)
         artic_adjusted_prices.add_adjusted_prices(instrument_code, adjusted_prices)
+
+
+if __name__ == '__main__':
+    generate_adjusted_prices()
