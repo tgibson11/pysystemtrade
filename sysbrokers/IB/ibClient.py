f40e8f675c8b37c5f7f85e3602f0582e7df8a28b
Todd Gibson <tgibson11@gmail.com>
2020-06-08 20:08:37 -0700

    Handle BOND & BILL asset classes

--- sysbrokers/IB/ibClient.py
+++ sysbrokers/IB/ibClient.py
@@ -532,17 +532,18 @@ def from_ib_positions_to_dict(raw_positions):
     """
     resolved_positions_dict = dict()
     position_methods = dict(STK = resolve_ib_stock_position, FUT = resolve_ib_future_position,
-                            CASH = resolve_ib_cash_position)
+                            CASH = resolve_ib_cash_position, BOND=resolve_ib_bond_position,
+                            BILL=resolve_ib_bill_position)
     for position in raw_positions:
         asset_class = position.contract.secType
         method = position_methods.get(asset_class, None)
         if method is None:
             raise Exception("Can't find asset class %s in methods dict" % asset_class)
-
-        resolved_position = method(position)
-        asset_class_list = resolved_positions_dict.get(asset_class, [])
-        asset_class_list.append(resolved_position)
-        resolved_positions_dict[asset_class] = asset_class_list
+        else:
+            resolved_position = method(position)
+            asset_class_list = resolved_positions_dict.get(asset_class, [])
+            asset_class_list.append(resolved_position)
+            resolved_positions_dict[asset_class] = asset_class_list
 
     return resolved_positions_dict
 
@@ -561,4 +562,16 @@ def resolve_ib_cash_position(position):
     return dict(account = position.account, symbol = position.contract.localSymbol,
                 expiry = "", multiplier = 1.0,
                 currency = position.contract.currency, position = position.position)
+
+
+def resolve_ib_bond_position(position):
+    return dict(account=position.account, symbol=position.contract.localSymbol,
+                expiry="", multiplier=1.0,
+                currency=position.contract.currency, position=position.position)
+
+
+def resolve_ib_bill_position(position):
+    return dict(account=position.account, symbol=position.contract.localSymbol,
+                expiry="", multiplier=1.0,
+                currency=position.contract.currency, position=position.position)
 
