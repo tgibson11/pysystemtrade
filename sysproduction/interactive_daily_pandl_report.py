fefbc20eabf901887c62aa9adf4ed8378b9544a7
Todd Gibson <tgibson11@gmail.com>
2020-06-13 13:44:17 -0700

    Created interactive P&L report

--- sysproduction/interactive_daily_pandl_report.py
+++ sysproduction/interactive_daily_pandl_report.py
@@ -1,15 +1,14 @@
-## Email roll data report
-## We also have a report function to email
-
 from sysproduction.diagnostic.reporting import run_report
 from syscore.objects import report_config
 
 
-def email_daily_pandl_report():
-
-
+def interactive_daily_pandl_report():
     pandl_report_config = report_config(title="One day P&L report",
-                                       function="sysproduction.diagnostic.profits.pandl_info",
-                                       output="email")
+                                        function="sysproduction.diagnostic.profits.pandl_info",
+                                        output="console")
 
-    run_report(pandl_report_config, calendar_days_back = 1)
+    run_report(pandl_report_config, calendar_days_back=1)
+
+
+if __name__ == '__main__':
+    interactive_daily_pandl_report()
