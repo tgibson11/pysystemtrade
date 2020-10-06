14dd104cd0f4a9fcc3123c677680f74451e0f0be
Todd Gibson <tgibson11@gmail.com>
2020-10-06 16:09:33 -0700

    Debugging

--- sysdata/mongodb/mongo_historic_orders.py
+++ sysdata/mongodb/mongo_historic_orders.py
@@ -48,6 +48,7 @@ class mongoGenericHistoricOrdersData(genericOrdersData):
     def add_order_to_data(self, order, ignore_duplication=False):
         # Duplicates will be overriden, so be careful
         order_id = order.order_id
+        print(f"Order fill time = {order.fill_datetime}")
         if self.get_order_with_orderid(order_id) is missing_order:
             return self._add_order_to_data_no_checking(order)
         if ignore_duplication:
@@ -58,7 +59,6 @@ class mongoGenericHistoricOrdersData(genericOrdersData):
     def _add_order_to_data_no_checking(self, order):
         # Duplicates will be overriden, so be careful
         mongo_record = order.as_dict()
-        print(f"Order fill time (adding) = {mongo_record['fill_datetime']}")
         self._mongo.collection.insert_one(mongo_record)
         return success
 
@@ -71,7 +71,6 @@ class mongoGenericHistoricOrdersData(genericOrdersData):
 
         order_class = self._order_class()
         order = order_class.from_dict(result_dict)
-        print(f"Order fill time (finding) = {order.fill_datetime}")
         return order
 
     def delete_order_with_orderid(self, order_id):
@@ -79,7 +78,6 @@ class mongoGenericHistoricOrdersData(genericOrdersData):
         return success
 
     def update_order_with_orderid(self, order_id, order):
-        print(f"Order fill time (updating) = {order.fill_datetime}")
         self._mongo.collection.update_one(dict(order_id=order_id), {'$set': order.as_dict()})
 
 
