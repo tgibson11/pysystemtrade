a85b471cbb3e75af6adf1cf402a83c3d6fed8976
Todd Gibson <tgibson11@gmail.com>
2020-10-19 06:29:46 -0700

    Only save 3 days of backtests; delete old backtests from backup to save space

--- sysproduction/clean_truncate_backtest_states.py
+++ sysproduction/clean_truncate_backtest_states.py
@@ -1,4 +1,5 @@
 from syscore.fileutils import delete_old_files_with_extension_in_pathname
+from sysproduction.backup_files import get_statefile_backup_directory
 from sysproduction.diagnostic.backtest_state import get_directory_store_backtests
 from sysproduction.data.get_data import dataBlob
 
@@ -22,12 +23,24 @@ class cleanTruncateBacktestStates:
             % directory_to_use
         )
         delete_old_files_with_extension_in_pathname(
-            directory_to_use, days_old=5, extension=".pck"
+            directory_to_use, days_old=3, extension=".pck"
         )
         delete_old_files_with_extension_in_pathname(
-            directory_to_use, days_old=5, extension=".yaml"
+            directory_to_use, days_old=3, extension=".yaml"
         )
 
+        # Also remove old backtests from backup directory to save space
+        directory_to_use = get_statefile_backup_directory
+        self.data.log.msg(
+            "Deleting old .pck and .yaml backtest state files in directory %s"
+            % directory_to_use
+        )
+        delete_old_files_with_extension_in_pathname(
+            directory_to_use, days_old=3, extension=".pck"
+        )
+        delete_old_files_with_extension_in_pathname(
+            directory_to_use, days_old=3, extension=".yaml"
+        )
 
 if __name__ == '__main__':
     clean_truncate_backtest_states()
