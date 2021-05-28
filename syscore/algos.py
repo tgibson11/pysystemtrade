51945bd0e332d96892966b8c6c52551e34f16963
todd <tgibson11@gmail.com>
2021-05-27 17:06:56 -0700

    Handle all weights == 0

--- syscore/algos.py
+++ syscore/algos.py
@@ -24,7 +24,11 @@ def calculate_weighted_average_with_nans(weights: list, data: list):
                     in zip(weights, data)]
 
     sum_safe_weights = sum(safe_weights)
-    renormalise = 1.0 /sum_safe_weights
+    if sum_safe_weights != 0:
+        renormalise = 1.0 / sum_safe_weights
+    else:
+        renormalise = 0
+
     weighted_value = [renormalise * weight * data_item for
                       weight, data_item in zip(safe_weights, data)]
 
