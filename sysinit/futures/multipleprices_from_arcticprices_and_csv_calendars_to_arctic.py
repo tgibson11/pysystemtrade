aa912dd9389cbcf61a030fdbfeb420256a2bc466
todd <tgibson11@gmail.com>
2021-07-11 08:06:43 -0700

    Add CHF

--- sysinit/futures/multipleprices_from_arcticprices_and_csv_calendars_to_arctic.py
+++ sysinit/futures/multipleprices_from_arcticprices_and_csv_calendars_to_arctic.py
@@ -169,5 +169,5 @@ if __name__ == "__main__":
     # process_multiple_prices_all_instruments(csv_multiple_data_path= csv_multiple_data_path,
     #                                         csv_roll_data_path = csv_roll_data_path)
 
-    for instrument in ['GOLD_micro']:
+    for instrument in ['CHF']:
         process_multiple_prices_single_instrument(instrument_code=instrument, ADD_TO_CSV=False)
