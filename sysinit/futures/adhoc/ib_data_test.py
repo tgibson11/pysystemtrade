d47455747b172018067b2b8aa76945008a275167
todd <3578666+tgibson11@users.noreply.github.com>
2023-03-03 06:23:41 -0700

    Add other market data types to IB data test

--- sysinit/futures/adhoc/ib_data_test.py
+++ sysinit/futures/adhoc/ib_data_test.py
@@ -1,3 +1,5 @@
+import datetime
+
 from ib_insync import Future
 from ib_insync import IB
 
@@ -6,7 +8,22 @@ ib.connect("127.0.0.1", 7496)
 ib.reqMarketDataType(3)
 contract = Future("MES", "202303", "CME")
 ib.qualifyContracts(contract)
+
+# Historical
 bars = ib.reqHistoricalData(contract, endDateTime='', durationStr='1 M', barSizeSetting='1 day', whatToShow='TRADES',
                             useRTH=True, formatDate=1)
 print(bars)
+
+# Ticks
+recent_time = datetime.datetime.now() - datetime.timedelta(seconds=60)
+tick_data = ib.reqHistoricalTicks(
+    contract, recent_time, "", 200, "BID_ASK", useRth=False
+)
+print(tick_data)
+
+# Streaming
+ib.reqMktData(contract, "", False, False)
+ticker = ib.ticker(contract)
+print(ticker)
+
 
