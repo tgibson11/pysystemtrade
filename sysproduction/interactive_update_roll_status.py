878c9e66989b75df69f427f3e7e103bcee24087e
todd <3578666+tgibson11@users.noreply.github.com>
2023-07-04 06:17:35 -0700

    Restore sorting of interactive roll updates (lost in merge)

--- sysproduction/interactive_update_roll_status.py
+++ sysproduction/interactive_update_roll_status.py
@@ -234,13 +234,21 @@ def get_list_of_instruments_to_auto_cycle(data: dataBlob, days_ahead: int = 10) 
     list_of_potential_instruments = (
         diag_prices.get_list_of_instruments_in_multiple_prices()
     )
-    instrument_list = [
-        instrument_code
+    instruments_and_days_to_expiry = [
+        (instrument_code, days_until_earliest_expiry(data, instrument_code))
         for instrument_code in list_of_potential_instruments
         if include_instrument_in_auto_cycle(
             data=data, instrument_code=instrument_code, days_ahead=days_ahead
         )
     ]
+
+    filtered_list = [
+        t for t in instruments_and_days_to_expiry if t[1] <= days_ahead
+    ]
+
+    sorted_list = sorted(filtered_list, key=lambda t: t[1])
+
+    instrument_list = [t[0] for t in sorted_list]
 
     print_with_landing_strips_around(
         "Identified following instruments that are near expiry %s"
