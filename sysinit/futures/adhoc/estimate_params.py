d3ab88cf7d4cab747f631522aaa0ca70cc2390ba
todd <3578666+tgibson11@users.noreply.github.com>
2023-10-23 09:07:04 -0700

    Fixes to parameter estimation script

--- sysinit/futures/adhoc/estimate_params.py
+++ sysinit/futures/adhoc/estimate_params.py
@@ -1,17 +1,18 @@
 import logging
 
+from sysdata.config.configdata import Config
 from systems.diagoutput import systemDiag
 from systems.provided.rob_system.run_system import futures_system
 
 logging.getLogger("arctic").setLevel(logging.ERROR)
 
-system = futures_system(config_filename="/home/todd/private/system_config.yaml")
+system = futures_system(config=Config("/home/todd/private/system_config.yaml"))
 
 system.config.use_forecast_scale_estimates = False
-system.config.use_forecast_weight_estimates = False
+system.config.use_forecast_weight_estimates = True
 system.config.use_forecast_div_mult_estimates = False
 system.config.use_instrument_weight_estimates = False
-system.config.use_instrument_div_mult_estimates = True
+system.config.use_instrument_div_mult_estimates = False
 
 sysdiag = systemDiag(system)
 
@@ -26,4 +27,4 @@ sysdiag.yaml_config_with_estimated_parameters('someyamlfile.yaml',
                                                           'instrument_weights',
                                                           'instrument_div_multiplier'])
 
-system.log.msg("Sharpe Ratio: " + str(system.accounts.portfolio().sharpe()))
+system.log.info("Sharpe Ratio: " + str(system.accounts.portfolio().sharpe()))
