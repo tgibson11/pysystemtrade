5522e4d401395241f98f8addf97072e0f94558b7
todd <3578666+tgibson11@users.noreply.github.com>
2023-10-23 09:13:38 -0700

    Estimate FDMs, not forecast weights

--- sysinit/futures/adhoc/estimate_params.py
+++ sysinit/futures/adhoc/estimate_params.py
@@ -9,8 +9,8 @@ logging.getLogger("arctic").setLevel(logging.ERROR)
 system = futures_system(config=Config("/home/todd/private/system_config.yaml"))
 
 system.config.use_forecast_scale_estimates = False
-system.config.use_forecast_weight_estimates = True
-system.config.use_forecast_div_mult_estimates = False
+system.config.use_forecast_weight_estimates = False
+system.config.use_forecast_div_mult_estimates = True
 system.config.use_instrument_weight_estimates = False
 system.config.use_instrument_div_mult_estimates = False
 
