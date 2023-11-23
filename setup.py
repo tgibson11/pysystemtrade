4980fd0af190d13f5d95758373c34226f008222a
todd <3578666+tgibson11@users.noreply.github.com>
2023-11-23 08:51:54 -0700

    Get setup.py to work

--- setup.py
+++ setup.py
@@ -77,8 +77,8 @@ setup(
         "Python framework for running systems as in Robert Carver's book Systematic Trading"
         " (https://www.systematicmoney.org/systematic-trading)"
     ),
-    license="GNU GPL v3",
-    keywords="systematic trading interactive brokers",
+    # license="GNU GPL v3",
+    # keywords="systematic trading interactive brokers",
     url="https://qoppac.blogspot.com/p/pysystemtrade.html",
     packages=find_packages(),
     package_data=package_data,
