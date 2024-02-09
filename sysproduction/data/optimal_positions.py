989a78bb9f3bf0cf267dd25609fac734d03a88d1
todd <3578666+tgibson11@users.noreply.github.com>
2024-02-09 07:16:06 -0700

    Filter stale instruments from optimal positions

--- sysproduction/data/optimal_positions.py
+++ sysproduction/data/optimal_positions.py
@@ -51,7 +51,10 @@ class dataOptimalPositions(productionDataLayerGeneric):
             use_strategy_name
         )
 
-        return list_of_instruments
+        stale_instruments = get_list_of_stale_instruments()
+        non_stale_instruments = [instr for instr in list_of_instruments if instr not in stale_instruments]
+
+        return non_stale_instruments
 
     def get_list_of_strategies_with_optimal_position(self) -> list:
         list_of_strategies = (
@@ -152,6 +155,9 @@ class dataOptimalPositions(productionDataLayerGeneric):
     @property
     def db_optimal_position_data(self) -> optimalPositionData:
         return self.data.db_optimal_position
+
+    def delete_optimal_positions_for_instrument(self, instrument_code):
+        self.db_optimal_position_data.delete_optimal_positions_for_instrument(instrument_code)
 
 
 POST_TAG_FOR_RAW_OPTIMAL_POSITION = "_raw"
