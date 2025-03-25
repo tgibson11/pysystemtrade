d2c4b9b497cf76bd5dc19fd38ad5acea0c7a9157
todd <3578666+tgibson11@users.noreply.github.com>
2025-03-25 09:33:58 -0700

    Update capital after prices, instead of before

--- sysproduction/run_daily.py
+++ sysproduction/run_daily.py
@@ -12,11 +12,11 @@ from sysproduction.startup import startup
 
 startup()
 
-run_capital_update()
 run_daily_fx_and_contract_updates()
 run_daily_price_updates()
 run_daily_update_multiple_adjusted_prices()
 
+run_capital_update()
 run_systems()
 run_strategy_order_generator()
 
