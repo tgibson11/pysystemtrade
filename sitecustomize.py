8dbba96cf14be5f3460b8b180a53e7e79deb29a0
todd <3578666+tgibson11@users.noreply.github.com>
2025-03-31 11:27:33 -0700

    Set pandas display options in sitecustomize.py

---
+++ sitecustomize.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Show all columns when printing a DataFrame
+pd.options.display.width = 0
+# Show more rows when printing a DataFrame
+# Default is 10: the first 5 and last 5
+pd.options.display.min_rows = 30
