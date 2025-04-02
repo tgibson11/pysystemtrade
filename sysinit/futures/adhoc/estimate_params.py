aace224d0ccdb5422cb431c14083c1de6df82c45
todd <3578666+tgibson11@users.noreply.github.com>
2025-04-02 08:45:21 -0700

    Formatting

--- sysinit/futures/adhoc/estimate_params.py
+++ sysinit/futures/adhoc/estimate_params.py
@@ -4,22 +4,18 @@ from matplotlib import pyplot as plt
 from systems.diagoutput import systemDiag
 from systems.provided.rob_system.run_system import futures_system
 
-system = futures_system(config_filename="/Users/Todd/PyCharmProjects/private/system_config.yaml")
+system = futures_system(
+    config_filename="/Users/Todd/PyCharmProjects/private/system_config.yaml"
+)
 
-system.config.use_forecast_scale_estimates = False
-system.config.use_forecast_weight_estimates = False
-system.config.use_forecast_div_mult_estimates = False
-system.config.use_instrument_weight_estimates = False
-system.config.use_instrument_div_mult_estimates = True
+# system.config.use_forecast_scale_estimates = False
+# system.config.use_forecast_weight_estimates = False
+# system.config.use_forecast_div_mult_estimates = True
+# system.config.use_instrument_weight_estimates = False
+# system.config.use_instrument_div_mult_estimates = True
 
 sysdiag = systemDiag(system)
-sysdiag.yaml_config_with_estimated_parameters('someyamlfile.yaml',
-                                              attr_names=['forecast_scalars',
-                                                          'forecast_weights',
-                                                          'forecast_div_multiplier',
-                                                          'forecast_mapping',
-                                                          'instrument_weights',
-                                                          'instrument_div_multiplier'])
+sysdiag.yaml_config_with_estimated_parameters('someyamlfile.yaml')
 
 system.log.info("Sharpe Ratio: " + str(system.accounts.portfolio().sharpe()))
 
