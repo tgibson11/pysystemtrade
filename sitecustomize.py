d60941848339ccad82f57987a08a40add1e6452b
todd <3578666+tgibson11@users.noreply.github.com>
2025-11-17 10:18:38 -0700

    Update option for printing DataFrame columns

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Show all columns when printing a DataFrame
-pd.options.display.width = 0
+pd.options.display.max_columns = None
 # Show more rows when printing a DataFrame
 # Default is 10: the first 5 and last 5
 pd.options.display.min_rows = 30
