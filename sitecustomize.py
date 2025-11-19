e2258e13025a661c8c35b913a7c7a0415b8b6846
todd <3578666+tgibson11@users.noreply.github.com>
2025-11-19 11:19:45 -0700

    Improve DataFrame display

--- sitecustomize.py
+++ sitecustomize.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 # Show all columns when printing a DataFrame
 pd.options.display.max_columns = None
+
 # Show more rows when printing a DataFrame
 # Default is 10: the first 5 and last 5
-pd.options.display.min_rows = 30
+pd.options.display.max_rows = None
+
+# Display all columns horizontally
+pd.options.display.width = 1000
