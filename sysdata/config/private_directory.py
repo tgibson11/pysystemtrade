7dc78770525a53adc61c18b67417d5a6f1fd05ec
Andy Geach <andy@bugorfeature.net>
2025-12-30 18:37:50 +0000

    deal with private config

--- sysdata/config/private_directory.py
+++ sysdata/config/private_directory.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 DEFAULT_PRIVATE_DIR = "private"
 PRIVATE_CONFIG_DIR_ENV_VAR = "PYSYS_PRIVATE_CONFIG_DIR"
@@ -13,3 +14,12 @@ def get_full_path_for_private_config(filename: str):
         private_config_path = f"{DEFAULT_PRIVATE_DIR}/{filename}"
 
     return private_config_path
+
+
+def get_private_config_dir():
+    if os.getenv(PRIVATE_CONFIG_DIR_ENV_VAR):
+        private_config_dir = Path(os.environ[PRIVATE_CONFIG_DIR_ENV_VAR])
+    else:
+        private_config_dir = Path(DEFAULT_PRIVATE_DIR)
+
+    return str(private_config_dir)
