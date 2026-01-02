77cf1da7e47307561b305bca110657b9e9819aed
Andy Geach <andy@bugorfeature.net>
2026-01-02 13:06:05 +0000

    revert

--- syscore/fileutils.py
+++ syscore/fileutils.py
@@ -1,18 +1,21 @@
 import glob
 import datetime
 import time
-
-# from importlib import import_module
-# from array import array
-from importlib.util import find_spec
-
-# import inspect
+from importlib import import_module
 import os
 from pathlib import Path
 from typing import List, Tuple
 
 from syscore.dateutils import SECONDS_PER_DAY
 
+# DO NOT DELETE: all these are unused: but are required to get the filename padding to work
+
+
+"""
+
+    FILES IN DIRECTORIES
+
+"""
 
 """
 
@@ -103,22 +106,68 @@ def resolve_path_and_filename_for_package(
     path_and_filename: str, separate_filename=None
 ) -> str:
     """
-    A way of resolving relative and absolute filenames
+    A way of resolving relative and absolute filenames, and dealing with awkward OS specific things
+
+    >>> resolve_path_and_filename_for_package("/home/rob/", "file.csv")
+    '/home/rob/file.csv'
+
+    >>> resolve_path_and_filename_for_package(".home.rob", "file.csv")
+    '/home/rob/file.csv'
+
+    >>> resolve_path_and_filename_for_package('C:\\home\\rob\\'', "file.csv")
+    'C:\\home\\rob\\file.csv'
+
+    >>> resolve_path_and_filename_for_package("syscore.tests", "file.csv")
+    '/home/rob/pysystemtrade/syscore/tests/file.csv'
+
+    >>> resolve_path_and_filename_for_package("/home/rob/file.csv")
+    '/home/rob/file.csv'
+
+    >>> resolve_path_and_filename_for_package(".home.rob.file.csv")
+    '/home/rob/file.csv'
+
+    >>> resolve_path_and_filename_for_package("C:\\home\\rob\\file.csv")
+    'C:\\home\\rob\\file.csv'
+
+    >>> resolve_path_and_filename_for_package("syscore.tests.file.csv")
+    '/home/rob/pysystemtrade/syscore/tests/file.csv'
+
     """
 
-    resolved = get_resolved_pathname(path_and_filename)
+    path_and_filename_as_list = transform_path_into_list(path_and_filename)
     if separate_filename is None:
-        path = Path(resolved)
+        (
+            path_as_list,
+            separate_filename,
+        ) = extract_filename_from_combined_path_and_filename_list(
+            path_and_filename_as_list
+        )
     else:
-        path = Path(resolved, separate_filename)
+        path_as_list = path_and_filename_as_list
 
-    return str(path)
+    resolved_pathname = get_pathname_from_list(path_as_list)
+
+    resolved_path_and_filename = os.path.join(resolved_pathname, separate_filename)
+
+    return resolved_path_and_filename
 
 
 def get_resolved_pathname(pathname: str) -> str:
     """
-    TODO
+    >>> get_resolved_pathname("/home/rob/")
+    '/home/rob'
+
+    >>> get_resolved_pathname(".home.rob")
+    '/home/rob'
+
+    >>> get_resolved_pathname('C:\\home\\rob\\'')
+    'C:\\home\\rob'
+
+    >>> get_resolved_pathname("syscore.tests")
+    '/home/rob/pysystemtrade/syscore/tests'
+
     """
+
     if isinstance(pathname, Path):
         # special case when already a Path
         pathname = str(pathname.absolute())
@@ -127,41 +176,158 @@ def get_resolved_pathname(pathname: str) -> str:
         # This is an ssh address for rsync - don't change
         return pathname
 
-    path = Path(pathname)
-    if path.is_absolute():
-        return str(path)
+    # Turn /,\ into . so system independent
+    path_as_list = transform_path_into_list(pathname)
+    resolved_pathname = get_pathname_from_list(path_as_list)
+
+    return resolved_pathname
+
+
+## something unlikely to occur naturally in a pathname
+RESERVED_CHARACTERS = "&!*"
+
+
+def transform_path_into_list(pathname: str) -> List[str]:
+    """
+    >>> path_as_list("/home/rob/test.csv")
+    ['', 'home', 'rob', 'test', 'csv']
+
+    >>> path_as_list("/home/rob/")
+    ['', 'home', 'rob']
+
+    >>> path_as_list(".home.rob")
+    ['', 'home', 'rob']
+
+    >>> path_as_list('C:\\home\\rob\\'')
+    ['C:', 'home', 'rob']
+
+    >>> path_as_list('C:\\home\\rob\\test.csv')
+    ['C:', 'home', 'rob', 'test', 'csv']
+
+    >>> path_as_list("syscore.tests.fileutils.csv")
+    ['syscore', 'tests', 'fileutils', 'csv']
+
+    >>> path_as_list("syscore.tests")
+    ['syscore', 'tests']
+
+    """
+
+    pathname_replace = add_reserved_characters_to_pathname(pathname)
+    path_as_list = pathname_replace.rsplit(RESERVED_CHARACTERS)
+
+    if path_as_list[-1] == "":
+        path_as_list.pop()
+
+    return path_as_list
+
+
+def add_reserved_characters_to_pathname(pathname: str) -> str:
+    pathname_replace = pathname.replace(".", RESERVED_CHARACTERS)
+    pathname_replace = pathname_replace.replace("/", RESERVED_CHARACTERS)
+    pathname_replace = pathname_replace.replace("\\", RESERVED_CHARACTERS)
+
+    return pathname_replace
+
+
+def extract_filename_from_combined_path_and_filename_list(
+    path_and_filename_as_list: list,
+) -> Tuple[list, str]:
+    """
+    >>> extract_filename_from_combined_path_and_filename_list(['home', 'rob','file', 'csv'])
+    (['home', 'rob'], 'file.csv')
+    """
+    ## need -2 because want extension
+    extension = path_and_filename_as_list.pop()
+    filename = path_and_filename_as_list.pop()
+
+    separate_filename = ".".join([filename, extension])
+
+    return path_and_filename_as_list, separate_filename
+
+
+def get_pathname_from_list(path_as_list: List[str]) -> str:
+    """
+    >>> get_pathname_from_list(['C:', 'home', 'rob'])
+    'C:\\home\\rob'
+    >>> get_pathname_from_list(['','home','rob'])
+    '/home/rob'
+    >>> get_pathname_from_list(['syscore','tests'])
+    '/home/rob/pysystemtrade/syscore/tests'
+    """
+    if path_as_list[0] == "":
+        # path_type_absolute
+        resolved_pathname = get_absolute_linux_pathname_from_list(path_as_list[1:])
+    elif is_windoze_path_list(path_as_list):
+        # windoze
+        resolved_pathname = get_absolute_windows_pathname_from_list(path_as_list)
     else:
-        return _resolve_path(pathname)
+        # relative
+        resolved_pathname = get_relative_pathname_from_list(path_as_list)
+
+    return resolved_pathname
 
 
-def _resolve_path(path: str) -> str:
+def is_windoze_path_list(path_as_list: List[str]) -> bool:
     """
-    Resolve a dotted path by iteratively treating trailing parts as a filename.
-    Returns the full resolved path (module directory + filename if present).
+    >>> is_windoze_path_list(['C:'])
+    True
+    >>> is_windoze_path_list(['wibble'])
+    False
     """
-    parts = path.split(".")
+    return path_as_list[0].endswith(":")
 
-    for i in range(len(parts), 0, -1):
-        module_parts = parts[:i]
-        file_parts = parts[i:]
 
-        candidate_module = ".".join(module_parts)
-        candidate_file = ".".join(file_parts) if file_parts else None
+def get_relative_pathname_from_list(path_as_list: List[str]) -> str:
+    """
 
-        try:
-            module_spec = find_spec(candidate_module)
-            if module_spec is not None and module_spec.origin:
-                module_path = Path(module_spec.origin).parent
+    >>> get_relative_pathname_from_list(['syscore','tests'])
+    '/home/rob/pysystemtrade/syscore/tests'
+    """
+    package_name = path_as_list[0]
+    paths_or_files = path_as_list[1:]
 
-                if candidate_file:
-                    if "." in candidate_file:
-                        return str(module_path / candidate_file)
-                else:
-                    return str(module_path)
-        except (ModuleNotFoundError, ValueError, AttributeError):
-            continue
+    if len(paths_or_files) == 0:
+        directory_name_of_package = os.path.dirname(
+            import_module(package_name).__file__
+        )
+        return directory_name_of_package
 
-    raise ModuleNotFoundError(f"Could not resolve path: {path}")
+    last_item_in_list = path_as_list.pop()
+    pathname = os.path.join(
+        get_relative_pathname_from_list(path_as_list), last_item_in_list
+    )
+
+    return pathname
+
+
+def get_absolute_linux_pathname_from_list(path_as_list: List[str]) -> str:
+    """
+    Returns the absolute pathname from a list
+
+    >>> get_absolute_linux_pathname_from_list(['home', 'rob'])
+    '/home/rob'
+    """
+    pathname = os.path.join(*path_as_list)
+    pathname = os.path.sep + pathname
+
+    return pathname
+
+
+def get_absolute_windows_pathname_from_list(path_as_list: list) -> str:
+    """
+    Test will fail on linux
+    >>> get_absolute_windows_pathname_from_list(['C:','home','rob'])
+    'C:\\home\\rob'
+    """
+    drive_part_of_path = path_as_list[0]
+    if drive_part_of_path.endswith(":"):
+        ## add back backslash
+        drive_part_of_path = drive_part_of_path.replace(":", ":\\")
+        path_as_list[0] = drive_part_of_path
+
+    pathname = os.path.join(*path_as_list)
+
+    return pathname
 
 
 """
@@ -179,13 +345,6 @@ def write_list_of_lists_as_html_table_in_file(file, list_of_lists: list):
         file.write("  </td></tr>")
 
     file.write("</table>")
-
-
-"""
-
-    FILES IN DIRECTORIES
-
-"""
 
 
 def files_with_extension_in_pathname(pathname: str, extension=".csv") -> List[str]:
--- syscore/tests/test_fileutils.py
+++ syscore/tests/test_fileutils.py
@@ -23,7 +23,6 @@ class TestFileUtilsUnix:
         actual = get_resolved_pathname("/home/rob/")
         assert actual == "/home/rob"
 
-    @pytest.mark.xfail(reason="Cannot work with new implementation")
     def test_resolve_path_absolute_dotted(self):
         actual = get_resolved_pathname(".home.rob")
         assert actual == "/home/rob"
@@ -33,9 +32,10 @@ class TestFileUtilsUnix:
         assert actual == f"{project_dir}/syscore/tests"
 
     def test_resolve_path_non_existent(self, project_dir):
-        with pytest.raises(ModuleNotFoundError):
-            get_resolved_pathname("syscore.testz")
+        actual = get_resolved_pathname("syscore.testz")
+        assert actual == f"{project_dir}/syscore/testz"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_dotted_dir_name(self, tmp_path):
         directory = tmp_path / "dir.name.with.dots"
         directory.mkdir()
@@ -44,6 +44,7 @@ class TestFileUtilsUnix:
         resolved_path = get_resolved_pathname(str(file))
         assert resolved_path == f"{tmp_path}/dir.name.with.dots/hello.txt"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_dotted_file_name(self, tmp_path):
         directory = tmp_path / "dir_name"
         directory.mkdir()
@@ -60,7 +61,6 @@ class TestFileUtilsUnix:
         actual = resolve_path_and_filename_for_package("/home/rob/file.csv")
         assert actual == "/home/rob/file.csv"
 
-    @pytest.mark.xfail(reason="Cannot work with new implementation")
     def test_resolve_package_combined_dotted(self):
         actual = resolve_path_and_filename_for_package(".home.rob.file.csv")
         assert actual == "/home/rob/file.csv"
@@ -81,12 +81,14 @@ class TestFileUtilsUnix:
         )
         assert actual == f"{project_dir}/syscore/tests/pricetestdata.csv"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_package_module_combined_dotted_filename(self, project_dir):
         actual = resolve_path_and_filename_for_package(
             "syscore.tests.price.test.data.csv"
         )
         assert actual == f"{project_dir}/syscore/tests/price.test.data.csv"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_resolve_path_and_filename_for_package_with_dotted_dir_name(
         self, tmp_path
     ):
@@ -99,6 +101,7 @@ class TestFileUtilsUnix:
         )
         assert resolved_path == f"{tmp_path}/dir.name.with.dots/hello.txt"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_resolve_path_and_filename_for_package_with_dotted_file_name(
         self, tmp_path
     ):
@@ -111,6 +114,83 @@ class TestFileUtilsUnix:
         )
         assert resolved_path == f"{tmp_path}/dir_name/dotted.filename.txt"
 
+    # No Separate filename
+
+    def test_csv_data(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "data.futures.csvconfig.instrumentconfig.csv"
+        )
+        assert actual == f"{project_dir}/data/futures/csvconfig/instrumentconfig.csv"
+
+    def test_logging(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "syslogging.logging_prod.yaml",
+        )
+        assert actual == f"{project_dir}/syslogging/logging_prod.yaml"
+
+    def test_config_defaults(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysdata.config.defaults.yaml",
+        )
+        assert actual == f"{project_dir}/sysdata/config/defaults.yaml"
+
+    def test_control_config_defaults(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "syscontrol.control_config.yaml",
+        )
+        assert actual == f"{project_dir}/syscontrol/control_config.yaml"
+
+    def test_private_config_dots(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private.private_config.yaml",
+        )
+        assert actual == f"{project_dir}/private/private_config.yaml"
+
+    def test_private_config_slash(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private/private_config.yaml",
+        )
+        assert actual == f"{project_dir}/private/private_config.yaml"
+
+    def test_strategy_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "systems.provided.rob_system.config.yaml",
+        )
+        assert actual == f"{project_dir}/systems/provided/rob_system/config.yaml"
+
+    def test_pickled_backtest(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private/backtests/fut_strategy_v1_8/20260101_210632_backtest.pck",
+        )
+        assert (
+            actual
+            == f"{project_dir}/private/backtests/fut_strategy_v1_8/20260101_210632_backtest.pck"
+        )
+
+    def test_ib_instrument_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.config.ib_config_futures.csv",
+        )
+        assert actual == f"{project_dir}/sysbrokers/IB/config/ib_config_futures.csv"
+
+    def test_ib_fx_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.config.ib_config_spot_FX.csv",
+        )
+        assert actual == f"{project_dir}/sysbrokers/IB/config/ib_config_spot_FX.csv"
+
+    def test_ib_trading_hours_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.ib_config_trading_hours.yaml",
+        )
+        assert actual == f"{project_dir}/sysbrokers/IB/ib_config_trading_hours.yaml"
+
+    def test_email_store_filename(self):
+        actual = resolve_path_and_filename_for_package(
+            "/home/rob/logs/email_store.log",
+        )
+        assert actual == "/home/rob/logs/email_store.log"
+
 
 @pytest.mark.skipif(sys.platform in ["linux", "darwin"], reason="Only runs on windows")
 class TestFileUtilsWindoze:
@@ -122,7 +202,6 @@ class TestFileUtilsWindoze:
         actual = get_resolved_pathname("C:\\home\\rob\\")
         assert actual == "C:\\home\\rob"
 
-    @pytest.mark.xfail(reason="Cannot work with new implementation")
     def test_resolve_path_absolute_dotted(self):
         actual = get_resolved_pathname(".home.rob")
         assert actual == "\\home\\rob"
@@ -132,9 +211,10 @@ class TestFileUtilsWindoze:
         assert actual == f"{project_dir}\\syscore\\tests"
 
     def test_resolve_path_non_existent(self, project_dir):
-        with pytest.raises(ModuleNotFoundError):
-            get_resolved_pathname("syscore.testz")
+        actual = get_resolved_pathname("syscore.testz")
+        assert actual == f"{project_dir}\\syscore\\testz"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_dotted_dir_name(self, tmp_path):
         directory = tmp_path / "dir.name.with.dots"
         directory.mkdir()
@@ -143,6 +223,7 @@ class TestFileUtilsWindoze:
         resolved_path = get_resolved_pathname(str(file))
         assert resolved_path == f"{tmp_path}\\dir.name.with.dots\\hello.txt"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_dotted_file_name(self, tmp_path):
         directory = tmp_path / "dir_name"
         directory.mkdir()
@@ -159,7 +240,6 @@ class TestFileUtilsWindoze:
         actual = resolve_path_and_filename_for_package("C:\\home\\rob\\file.csv")
         assert actual == "C:\\home\\rob\\file.csv"
 
-    @pytest.mark.xfail(reason="Cannot work with new implementation")
     def test_resolve_package_combined_dotted(self):
         actual = resolve_path_and_filename_for_package(".home.rob.file.csv")
         assert actual == "\\home\\rob\\file.csv"
@@ -174,12 +254,14 @@ class TestFileUtilsWindoze:
         )
         assert actual == f"{project_dir}\\syscore\\tests\\pricetestdata.csv"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_package_module_combined_dotted_filename(self, project_dir):
         actual = resolve_path_and_filename_for_package(
             "syscore.tests.price.test.data.csv"
         )
         assert actual == f"{project_dir}\\syscore\\tests\\price.test.data.csv"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_resolve_path_and_filename_for_package_with_dotted_dir_name(
         self, tmp_path
     ):
@@ -192,6 +274,7 @@ class TestFileUtilsWindoze:
         )
         assert resolved_path == f"{tmp_path}\\dir.name.with.dots\\hello.txt"
 
+    @pytest.mark.xfail(reason="Cannot work with existing implementation")
     def test_resolve_resolve_path_and_filename_for_package_with_dotted_file_name(
         self, tmp_path
     ):
@@ -203,3 +286,88 @@ class TestFileUtilsWindoze:
             f"{tmp_path}\\dir_name\\dotted.filename.txt"
         )
         assert resolved_path == f"{tmp_path}\\dir_name\\dotted.filename.txt"
+
+    # No Separate filename
+
+    def test_csv_data(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "data.futures.csvconfig.instrumentconfig.csv"
+        )
+        assert (
+            actual == f"{project_dir}\\data\\futures\\csvconfig\\instrumentconfig.csv"
+        )
+
+    def test_logging(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "syslogging.logging_prod.yaml",
+        )
+        assert actual == f"{project_dir}\\syslogging\\logging_prod.yaml"
+
+    def test_config_defaults(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysdata.config.defaults.yaml",
+        )
+        assert actual == f"{project_dir}\\sysdata\\config\\defaults.yaml"
+
+    def test_control_config_defaults(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "syscontrol.control_config.yaml",
+        )
+        assert actual == f"{project_dir}\\syscontrol\\control_config.yaml"
+
+    def test_private_config_dots(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private.private_config.yaml",
+        )
+        assert actual == f"{project_dir}\\private\\private_config.yaml"
+
+    def test_private_config_slash(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private/private_config.yaml",
+        )
+        assert actual == f"{project_dir}\\private\\private_config.yaml"
+
+    def test_strategy_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "systems.provided.rob_system.config.yaml",
+        )
+        assert actual == f"{project_dir}\\systems\\provided\\rob_system\\config.yaml"
+
+    def test_pickled_backtest(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "private/backtests/fut_strategy_v1_8/20260101_210632_backtest.pck",
+        )
+        assert (
+            actual
+            == f"{project_dir}\\private\\backtests\\fut_strategy_v1_8\\20260101_210632_backtest.pck"
+        )
+
+    def test_ib_instrument_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.config.ib_config_futures.csv",
+        )
+        assert actual == f"{project_dir}\\sysbrokers\\IB\\config\\ib_config_futures.csv"
+
+    def test_ib_fx_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.config.ib_config_spot_FX.csv",
+        )
+        assert actual == f"{project_dir}\\sysbrokers\\IB\\config\\ib_config_spot_FX.csv"
+
+    def test_ib_trading_hours_config(self, project_dir):
+        actual = resolve_path_and_filename_for_package(
+            "sysbrokers.IB.ib_config_trading_hours.yaml",
+        )
+        assert actual == f"{project_dir}\\sysbrokers\\IB\\ib_config_trading_hours.yaml"
+
+    def test_email_store_filename(self):
+        actual = resolve_path_and_filename_for_package(
+            "C:\\home\\rob\\logs\\email_store.log",
+        )
+        assert actual == "C:\\home\\rob\\logs\\email_store.log"
+
+    def test_convert_email_store_filename(self):
+        actual = resolve_path_and_filename_for_package(
+            "/home/rob/logs/email_store.log",
+        )
+        assert actual == "\\home\\rob\\logs\\email_store.log"
