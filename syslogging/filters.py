d744a6d57f59a42fc005729512ae971489326cba
Andy Geach <andy@bugorfeature.net>
2026-02-09 16:17:40 +0000

    allow any records not belonging to 'ib_insync'

--- syslogging/filters.py
+++ syslogging/filters.py
@@ -10,13 +10,15 @@ class IBFilter(logging.Filter):
         super().__init__("ib_insync")
 
     def filter(self, record):
-        # if msg starts with 'Warning', then set the level to WARNING and allow
-        if record.msg.startswith("Warning"):
-            record.levelname = "WARNING"
-            record.levelno = 30
-            return True
-        # don't allow INFO or lower
-        if record.levelno < 25:
-            return False
+        # for all records with name starting 'ib_insync'
+        if record.name.startswith(self.name):
+            # if msg starts with 'Warning', then set the level to WARNING and allow
+            if record.msg.startswith("Warning"):
+                record.levelname = "WARNING"
+                record.levelno = 30
+                return True
+            # don't allow INFO or lower
+            if record.levelno < 25:
+                return False
         # otherwise allow
         return True
